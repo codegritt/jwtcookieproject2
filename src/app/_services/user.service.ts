f902bb83c9a58dfcea9cf53e16cdedc2c4f6adcb
gokul <gokulchandan18@gmail.com>
2022-03-29 13:20:34 +0530

    commit

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,8 +1,8 @@
 ﻿import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { environment } from '@environments/environment';
-import { User } from '@app/_models';
+import { environment } from '../../environments/environment.prod';
+import { User } from'../../app/_models';
 
 const baseUrl = `${environment.apiUrl}/users`;
 
