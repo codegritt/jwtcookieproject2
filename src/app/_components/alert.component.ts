edd73d4d105efca8475aa8a5b6e8f9657430be4e
gokul <gokulchandan18@gmail.com>
2022-03-29 13:26:21 +0530

    commit

--- src/app/_components/alert.component.ts
+++ src/app/_components/alert.component.ts
@@ -2,7 +2,7 @@
 import { Router, NavigationStart } from '@angular/router';
 import { Subscription } from 'rxjs';
 
-import { Alert, AlertType } from '../../app/_services';
+import { Alert, AlertType } from '../../app/_models';
 import { AlertService } from '../../app/_services';
 
 @Component({ selector: 'alert', templateUrl: 'alert.component.html' })
